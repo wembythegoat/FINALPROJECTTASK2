897ff744004b31225306a1185d8e7a7b043799c2
wembythegoat <aswinthsinnathamby@gmail.com>
2024-11-20 15:52:44 -0500

    Second commit

---
+++ Average.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Average {
+    private int[] data;
+    private double mean;
+
+    public Average() {
+     Scanner sc = new Scanner(System.in);
+        data = new int[5];
+        for (int i = 0; i < data.length; i++) {
+            System.out.print("Enter a score: " + (i + 1));
+            data [i] = sc.nextInt();
+        }
+        calculateMean();
+        selectionSort();
+    }
+    public void calculateMean() {
+        double total = 0;
+        for (int score:data) {
+            total += score;
+        }
+        mean = total/ data.length;
+    }
+    public String toString() {
+        String result = "Descending order of the data";
+        for (int i = 0; i < data.length - 1; i++) {
+            result += data[i] + " ";
+        }
+        result += "\nMean: " + mean;
+        return result;
+    }
+    public void selectionSort() {
+        for (int i = 0; i < data.length - 1; i++) {
+            int maxIndex = i;
+            for (int k = i + 1; k < data.length; k++) {
+                if (data [k] > data[maxIndex]) {
+                    maxIndex = k;
+                }
+            }
+            
+        }
+
+    }
+}
